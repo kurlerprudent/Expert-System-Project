699bebec2970271db725ffc232cdc274789a6a60
kurlerprudent <osarkodie@st.ug.edu.gh>
2025-02-05 07:58:52 +0000

    Added the keys

--- src/libs/supabaseClient.ts
+++ src/libs/supabaseClient.ts
@@ -1,7 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL!
-const supabaseKey = process.env.SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://miaeuoknkostaopiidwb.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pYWV1b2tua29zdGFvcGlpZHdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2MTczNjIsImV4cCI6MjA1NDE5MzM2Mn0.trKy3d_phNPc8Ha5Ntd9NtNSWhtwDCpcqgyKTC-x1o0'
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
